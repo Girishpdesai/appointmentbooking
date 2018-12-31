4d109b6df07a4a64abb79b6d2fdc765ae746389f
Girish Prabhudesai <prabhudesaigiri@gmail.com>
2018-12-31 11:44:00 -0500

    fixed issues related to object mapping

--- src/main/java/com/cgi/appointmentbooking/assembler/AppointmentResourceAssembler.java
+++ src/main/java/com/cgi/appointmentbooking/assembler/AppointmentResourceAssembler.java
@@ -1,7 +1,11 @@
 package com.cgi.appointmentbooking.assembler;
 
+import org.springframework.stereotype.Component;
+
 import com.cgi.appointmentbooking.domain.Appointment;
 import com.cgi.appointmentbooking.presentation.AppointmentResource;
+
+@Component
 
 public class AppointmentResourceAssembler extends ResourceAssembler<Appointment, AppointmentResource>{
 	public AppointmentResource toResource(Appointment appointment) {
